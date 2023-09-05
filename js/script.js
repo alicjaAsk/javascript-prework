9c53e41a21bc05804d42ae13a986a021d5497d46
Alicja SK <a.szmydkrysiak@gmail.com>
2023-09-05 12:09:57 +0200

    game2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+let playerInput = prompt("Wybierz swój ruch: 1- kamień, 2- papier; 3- nożyce.");
 
-console.log("Gracz wpisał: " + playerInput);
-
+console.log("Ja wybrałam: " + playerInput);
 let playerMove = "nieznany ruch";
 
 if (playerInput == "1") {
@@ -11,5 +10,33 @@ if (playerInput == "1") {
 } else if (playerInput == "3") {
   playerMove = "nożyce";
 }
+printMessage("Mój ruch to: " + playerMove);
 
-console.log("Twój ruch to: " + playerMove);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log("wylosowana liczba to: " + randomNumber);
+let computerMove = "nieznany ruch";
+
+if (randomNumber == "1") {
+  computerMove = "kamień";
+} else if (randomNumber == "2") {
+  computerMove = "papier";
+} else {
+  computerMove = "nożyce";
+}
+printMessage("Twój ruch to: " + computerMove);
+
+if (
+  (computerMove == "kamień" && playerMove == "papier") ||
+  (computerMove == "papier" && playerMove == "nożyce") ||
+  (computerMove == "nożyce" && playerMove == "kamień")
+) {
+  printMessage("Tą rozgrywkę wygrywam ja!");
+} else if (
+  (computerMove == "kamień" && playerMove == "nożyce") ||
+  (computerMove == "papier" && playerMove == "kamień") ||
+  (computerMove == "nożyce" && playerMove == "papier")
+) {
+  printMessage("Tą rozgrywkę wygrywasz Ty!");
+} else {
+  printMessage("Remis");
+}
